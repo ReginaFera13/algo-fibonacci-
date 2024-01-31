1c3c2bcc6dbefab28817a09ed979691ad46cd2bf
SHAWN_SMITH <141180011+ReginaFera13@users.noreply.github.com>
2024-01-31 16:43:17 -0500

    fibonacci solved

--- fibonacci.js
+++ fibonacci.js
@@ -1,5 +1,16 @@
 function fibonacci(num) {
-  return 0;
+  let prev = 0;
+  let curr = 1;
+  let result = 0;
+  for (i = 1; i <= num; i++) {
+    result = curr;
+    curr += prev;
+    prev = curr - prev;
+  }
+  
+  return result;
 }
+
+console.log(fibonacci(7))
 
 module.exports = fibonacci;
